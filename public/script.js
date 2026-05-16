66c09cef366b8212948363dd5036ae6d4531bc31
Marina Miranda <miranda.contatopro@gmail.com>
2026-05-16 13:48:28 -0300

    adiciona html, data produtos e elementos DOM

--- public/script.js
+++ public/script.js
@@ -2,24 +2,90 @@ const data = {
     "produtos": [
       {
         "id": 1,
-        "nome": "Smartphone Galaxy S23",
-        "preco": 3499.90,
-        "categoria": "Celulares",
-        "imagem": "https://example.com/imagens/galaxy-s23.jpg",
-        "descricao": "Smartphone com 128GB de armazenamento, câmera de alta resolução e excelente desempenho.",
+        "nome": "Ração Preferida",
+        "preco": 189.90,
+        "categoria": "Alimentação",
+        "imagem": "10002310000111_1775767302891.jpg",
+        "descricao": "Golden Fórmula para Cães Senior Sabor Frango e Arroz, 15kg.",
         "emEstoque": true
       },
       {
         "id": 2,
-        "nome": "Notebook Dell Inspiron 15",
-        "preco": 4599.00,
-        "categoria": "Notebooks",
-        "imagem": "https://example.com/imagens/dell-inspiron-15.jpg",
-        "descricao": "Notebook com processador Intel i7, 16GB de RAM e SSD de 512GB, ideal para trabalho e estudos.",
+        "nome": "Osso para roer e relaxar",
+        "preco": 20.00,
+        "categoria": "Petiscos",
+        "imagem": "1694547499205.jpg",
+        "descricao": "Osso Spike Fêmur Suíno para Cães",
         "emEstoque": false
+      },
+       {
+        "id": 3,
+        "nome": "Para momentos de treino e distração",
+        "preco": 34.90,
+        "categoria": "Petiscos",
+        "imagem": "1701974898933.jpg",
+        "descricao": "Bifinho Selections For Pets Strip Mini para Cães Sabor Carne 500 g.",
+        "emEstoque": true
+      },
+       {
+        "id": 4,
+        "nome": "Para sonhar bem Cãofortavel",
+        "preco": 349.90,
+        "categoria": "Cama",
+        "imagem": "1567172360754.jpg",
+        "descricao": "Cama Lisa Oval Sarja para Cães Cinza.",
+        "emEstoque": true
+      },
+       {
+        "id": 5,
+        "nome": "Para ficar quentinho no frio",
+        "preco": 79.90,
+        "categoria": "Cama",
+        "imagem": "1655903978691.jpg",
+        "descricao": "Cobertor Griff Dog para Cães Quentinho.",
+        "emEstoque": true
+      },
+       {
+        "id": 6,
+        "nome": "Água sempre fresquinha",
+        "preco": 49.90,
+        "categoria": "Comedouro",
+        "imagem": "1687378750389.jpg",
+        "descricao": "Comedouro Chalesco Bowl Cerâmica Magno para Cães",
+        "emEstoque": true
+      },
+       {
+        "id": 7,
+        "nome": "A melhor hora do dia",
+        "preco": 149.90,
+        "categoria": "Coleiras",
+        "imagem": "1570459045990.jpg",
+        "descricao": "Peitoralem nylon Zee.Dog Skull para Cães G.",
+        "emEstoque": true
+      },
+       {
+        "id": 8,
+        "nome": "Xô pulgas e carrapatos",
+        "preco": 199.90,
+        "categoria": "Coleiras",
+        "imagem": "1627415022942.jpg",
+        "descricao": "Coleira Antipulgas e Carrapatos Seresto para Cães acima de 8 Kg Elanco.",
+        "emEstoque": true
       }
     ]
   }
+
+  // elementos Dom:
+const productList = document.getElementById("product-list"); // pega a div dos cards
+const productDetails = document.getElementById("product-details"); // pega a div de detalhes
+const searchInput = document.querySelector("#search"); // pega o campo de busca
+const categorySelect = document.querySelector("#category"); // pega o select de categorias
+const btnRender = document.getElementById("btnRender"); // pega o botão Buscar produto
+
+// Formatação do preço
+function formatPrice(preco) {
+    return "R$ " + preco.toFixed(2); // toFixed(2) usa 2 casas decimais
+}
 
 
   
